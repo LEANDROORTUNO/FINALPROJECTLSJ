1d2fa8043c4b231ffbc5fb7e68a0f8910e2b0e38
LEANDROORTUNO <menprogramacion@gmail.com>
2025-11-25 16:02:57 -0400

    Parte leo

--- Tests/test_CoefiB.py
+++ Tests/test_CoefiB.py
@@ -1,6 +1,18 @@
 from CodigoProyecto import CoefiB as CB
 import math
 
-def test_CoefiB():
-    CoefB = CB.main()
+def test_CoefiB1TX():
+    CoefB = CB.main1TX()
+    assert math.isclose(CoefB, 0), f"La prueba falló. Esperado: 1.0, Obtenido: {CoefB}"
+    
+def test_CoefiB1VX():
+    CoefB = CB.main1VX()
+    assert math.isclose(CoefB, 1), f"La prueba falló. Esperado: 1.0, Obtenido: {CoefB}"
+    
+def test_CoefiB2TX():
+    CoefB = CB.main2TX()
+    assert math.isclose(CoefB, 0), f"La prueba falló. Esperado: 1.0, Obtenido: {CoefB}"
+    
+def test_CoefiB2VX():
+    CoefB = CB.main2VX()
     assert math.isclose(CoefB, 0), f"La prueba falló. Esperado: 1.0, Obtenido: {CoefB}"
