a26c9a3ab7503597eb795924c76ae34db567573c
jos2006 <jos2006@githup.com>
2025-11-25 16:41:06 -0400

    parte jose

--- CodigoProyecto/CoefiA.py
+++ CodigoProyecto/CoefiA.py
@@ -33,7 +33,8 @@ def delta(n, Sumx, Sumx2):
 def CoeficienteA(Sumx, Sumx2, Sumy, Sumxy, Delta):
     return (Sumy * Sumx2 - Sumxy * Sumx)/Delta
 
-def main():
+# SOLO T y X
+def main1TX():
     #listas de prueba:
 	tabla1x = [1.000 , 0.951 , 0.809 , 0.588 , 0.309 , 0.000]
 	tabla1t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
@@ -47,7 +48,191 @@ def main():
 	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
 	return CofA
 
+def main2TX():
+    #listas de prueba:
+	tabla2x = [0.707 , 0.410 , 0.060 , -0.298 , -0.618, -0.856]
+	tabla2t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla2x)
+	Sumx = sumx(tabla2x)
+	Sumx2 = sumx2(tabla2x)
+	Sumy = sumy(tabla2t)
+	Sumxy = sumxy(tabla2x, tabla2t)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+	
+
+def main3TX():
+    #listas de prueba:
+	tabla3x = [0.0 , 0.710 , 1.327 , 1.772 , 1.986 , 1.941]
+	tabla3t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla3x)
+	Sumx = sumx(tabla3x)
+	Sumx2 = sumx2(tabla3x)
+	Sumy = sumy(tabla3t)
+	Sumxy = sumxy(tabla3x, tabla3t)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main4TX():
+    #listas de prueba:
+	tabla4x = [0.0 , 0.860 , 1.552, 1.944 , 1.958, 1.591]
+	tabla4t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla4x)
+	Sumx = sumx(tabla4x)
+	Sumx2 = sumx2(tabla4x)
+	Sumy = sumy(tabla4t)
+	Sumxy = sumxy(tabla4x, tabla4t)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main5TX():
+    #listas de prueba:
+	tabla5x = [2.000 , 1.806 , 1.261 , 0.471 , -0.410 , -1.211]
+	tabla5t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla5x)
+	Sumx = sumx(tabla5x)
+	Sumx2 = sumx2(tabla5x)
+	Sumy = sumy(tabla5t)
+	Sumxy = sumxy(tabla5x, tabla5t)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main6TX():
+    #listas de prueba:
+	tabla6x = [-1.000 , -0.809 , -0.309 , 0.309 , 0.809 , 1.000]
+	tabla6t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla6x)
+	Sumx = sumx(tabla6x)
+	Sumx2 = sumx2(tabla6x)
+	Sumy = sumy(tabla6t)
+	Sumxy = sumxy(tabla6x, tabla6t)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+# SOLO V Y X
+
+def main1VX():
+    #listas de prueba:
+	tabla1x = [1.000 , 0.951 , 0.809 , 0.588 , 0.309 , 0.000]
+	tabla1v = [0.000 , -0.485 , -0.923 , -1.271 , -1.491 , -1.571]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla1x)
+	Sumx = sumx(tabla1x)
+	Sumx2 = sumx2(tabla1x)
+	Sumy = sumy(tabla1v)
+	Sumxy = sumxy(tabla1x, tabla1v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
 
 
+def main2VX():
+    #listas de prueba:
+	tabla2x = [0.707 , 0.410 , 0.060 , -0.298 , -0.618, -0.856]
+	tabla2v = [-1.283 , -1.654 , -1.811 , -1.731 , -1.427 , -0.936]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla2x)
+	Sumx = sumx(tabla2x)
+	Sumx2 = sumx2(tabla2x)
+	Sumy = sumy(tabla2v)
+	Sumxy = sumxy(tabla2x, tabla2v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main3VX():
+    #listas de prueba:
+	tabla3x = [0.0 , 0.710 , 1.327 , 1.772 , 1.986 , 1.941]
+	tabla3v = [3.628 , 3.392, 2.714, 1.683 , 0.483 , -0.873]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla3x)
+	Sumx = sumx(tabla3x)
+	Sumx2 = sumx2(tabla3x)
+	Sumy = sumy(tabla3v)
+	Sumxy = sumxy(tabla3x, tabla3v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main4VX():
+    #listas de prueba:
+	tabla4x = [0.0 , 0.860 , 1.552, 1.944 , 1.958, 1.591]
+	tabla4v = [4.443, 4.012, 2.801, 1.047, -0.910, -2.691]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla4x)
+	Sumx = sumx(tabla4x)
+	Sumx2 = sumx2(tabla4x)
+	Sumy = sumy(tabla4v)
+	Sumxy = sumxy(tabla4x, tabla4v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main5VX():
+    #listas de prueba:
+	tabla5x = [2.000 , 1.806 , 1.261 , 0.471 , -0.410 , -1.211]
+	tabla5v = [0.0 , -1.910 , -3.448 , -4.318 , -4.349 , -3.535]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla5x)
+	Sumx = sumx(tabla5x)
+	Sumx2 = sumx2(tabla5x)
+	Sumy = sumy(tabla5v)
+	Sumxy = sumxy(tabla5x, tabla5v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+def main6VX():
+    #listas de prueba:
+	tabla6x = [-1.000 , -0.809 , -0.309 , 0.309 , 0.809 , 1.000]
+	tabla6v = [0.000 , 1.847 , 2.988 , 2.988 , 1.847 , 0.000]
+	#Todas las listas proporcionadas por el Ing.Gabriel son de tamaño 6
+	n = len(tabla6x)
+	Sumx = sumx(tabla6x)
+	Sumx2 = sumx2(tabla6x)
+	Sumy = sumy(tabla6v)
+	Sumxy = sumxy(tabla6x, tabla6v)
+	Delta = delta(n, Sumx, Sumx2)
+	CofA = (int)(CoeficienteA(Sumx, Sumx2,  Sumy, Sumxy, Delta))
+	return CofA
+
+# MAIN
+def main():
+    print("TABLA 1 TX")
+    print(main1TX())
+    print("TABLA 2 TX")
+    print(main2TX())
+    print("TABLA 3 TX")
+    print(main3TX())
+    print("TABLA 4 TX")
+    print(main4TX())
+    print("TABLA 5 TX")
+    print(main5TX())
+    print("TABLA 6 TX")
+    print(main6TX())
+    
+    print("TABLA 1 VX")
+    print(main1VX())
+    print("TABLA 2 VX")
+    print(main2VX())
+    print("TABLA 3 VX")
+    print(main3VX())
+    print("TABLA 4 VX")
+    print(main4VX())
+    print("TABLA 5 VX")
+    print(main5VX())
+    print("TABLA 6 VX")
+    print(main6VX())
+    
 if __name__ == "__main__":
     main()
--- CodigoProyecto/CoefiB.py
+++ CodigoProyecto/CoefiB.py
@@ -1,3 +1,4 @@
+
 import math
 
 def suma_x(listaX):
@@ -38,28 +39,185 @@ def CoefB(n, Suma_x, Suma_x2, Suma_y, Suma_xy, Delta):
     return (n * Suma_xy - Suma_x * Suma_y) / Delta
 
 
-def main():
+# SOLO T y X
+
+def main1TX():
     tabla1x = [1.000 , 0.951 , 0.809 , 0.588 , 0.309 , 0.000]
-    
     tabla1t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
-
     n = len(tabla1x)
-
     Suma_x = suma_x(tabla1x)
-
     Suma_x2 = suma_x2(tabla1x)
-
     Suma_y = suma_y(tabla1t)
-
     Suma_xy = suma_xy(tabla1x, tabla1t)
-
     Delta = delta(n, Suma_x, Suma_x2)
-
     CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
 
+def main2TX():
+    tabla2t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+    tabla2x = [0.707 , 0.410 , 0.060 , -0.298 , -0.618, -0.856]
+    n = len(tabla2x)
+    Suma_x = suma_x(tabla2x)
+    Suma_x2 = suma_x2(tabla2x)
+    Suma_y = suma_y(tabla2t)
+    Suma_xy = suma_xy(tabla2x, tabla2t)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main3TX():
+    tabla3t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+    tabla3x = [0.0 , 0.710 , 1.327 , 1.772 , 1.986 , 1.941]
+    n = len(tabla3x)
+    Suma_x = suma_x(tabla3x)
+    Suma_x2 = suma_x2(tabla3x)
+    Suma_y = suma_y(tabla3t)
+    Suma_xy = suma_xy(tabla3x, tabla3t)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
     return CofB
 
 
-if __name__ == "__main__":
+def main4TX():
+    tabla4x = [0.0 , 0.860 , 1.552, 1.944 , 1.958, 1.591]
+    tabla4t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+    n = len(tabla4x)
+    Suma_x = suma_x(tabla4x)
+    Suma_x2 = suma_x2(tabla4x)
+    Suma_y = suma_y(tabla4t)
+    Suma_xy = suma_xy(tabla4x, tabla4t)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
 
-    main()
+def main5TX():
+    tabla5x = [2.000 , 1.806 , 1.261 , 0.471 , -0.410 , -1.211]
+    tabla5t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+    n = len(tabla5x)
+    Suma_x = suma_x(tabla5x)
+    Suma_x2 = suma_x2(tabla5x)
+    Suma_y = suma_y(tabla5t)
+    Suma_xy = suma_xy(tabla5x, tabla5t)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main6TX():
+    tabla6x = [-1.000 , -0.809 , -0.309 , 0.309 , 0.809 , 1.000]
+    tabla6t = [0.000 , 0.200 , 0.400 , 0.600 , 0.800 , 1.000]
+    n = len(tabla6x)
+    Suma_x = suma_x(tabla6x)
+    Suma_x2 = suma_x2(tabla6x)
+    Suma_y = suma_y(tabla6t)
+    Suma_xy = suma_xy(tabla6x, tabla6t)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+
+
+# SOLO V y X
+def main1VX():
+    tabla1x = [1.000 , 0.951 , 0.809 , 0.588 , 0.309 , 0.000]
+    tabla1v = [0.000 , -0.485 , -0.923 , -1.271 , -1.491 , -1.571]
+    n = len(tabla1x)
+    Suma_x = suma_x(tabla1x)
+    Suma_x2 = suma_x2(tabla1x)
+    Suma_y = suma_y(tabla1v)
+    Suma_xy = suma_xy(tabla1x, tabla1v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main2VX():
+    tabla2x = [0.707 , 0.410 , 0.060 , -0.298 , -0.618, -0.856]
+    tabla2v = [-1.283 , -1.654 , -1.811 , -1.731 , -1.427 , -0.936]
+    n = len(tabla2x)
+    Suma_x = suma_x(tabla2x)
+    Suma_x2 = suma_x2(tabla2x)
+    Suma_y = suma_y(tabla2v)
+    Suma_xy = suma_xy(tabla2x, tabla2v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main3VX():
+    tabla3x = [0.0 , 0.710 , 1.327 , 1.772 , 1.986 , 1.941]
+    tabla3v = [3.628 , 3.392, 2.714, 1.683 , 0.483 , -0.873]
+    n = len(tabla3x)
+    Suma_x = suma_x(tabla3x)
+    Suma_x2 = suma_x2(tabla3x)
+    Suma_y = suma_y(tabla3v)
+    Suma_xy = suma_xy(tabla3x, tabla3v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main4VX():
+    tabla4x = [0.0 , 0.860 , 1.552, 1.944 , 1.958, 1.591]
+    tabla4v = [4.443, 4.012, 2.801, 1.047, -0.910, -2.691]
+    n = len(tabla4x)
+    Suma_x = suma_x(tabla4x)
+    Suma_x2 = suma_x2(tabla4x)
+    Suma_y = suma_y(tabla4v)
+    Suma_xy = suma_xy(tabla4x, tabla4v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+def main5VX():
+    tabla5x = [2.000 , 1.806 , 1.261 , 0.471 , -0.410 , -1.211]
+    tabla5v = [0.0 , -1.910 , -3.448 , -4.318 , -4.349 , -3.535]
+    n = len(tabla5x)
+    Suma_x = suma_x(tabla5x)
+    Suma_x2 = suma_x2(tabla5x)
+    Suma_y = suma_y(tabla5v)
+    Suma_xy = suma_xy(tabla5x, tabla5v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+
+
+def main6VX():
+    tabla6x = [-1.000 , -0.809 , -0.309 , 0.309 , 0.809 , 1.000]
+    tabla6v = [0.000 , 1.847 , 2.988 , 2.988 , 1.847 , 0.000]
+    n = len(tabla6x)
+    Suma_x = suma_x(tabla6x)
+    Suma_x2 = suma_x2(tabla6x)
+    Suma_y = suma_y(tabla6v)
+    Suma_xy = suma_xy(tabla6x, tabla6v)
+    Delta = delta(n, Suma_x, Suma_x2)
+    CofB = (int)(CoefB(n, Suma_x, Suma_x2,  Suma_y, Suma_xy, Delta))
+    return CofB
+
+
+# main()
+def main():
+    print("TABLA 1 TX")
+    print(main1TX())
+    print("TABLA 2 TX")
+    print(main2TX())
+    print("TABLA 3 TX")
+    print(main3TX())
+    print("TABLA 4 TX")
+    print(main4TX())
+    print("TABLA 5 TX")
+    print(main5TX())
+    print("TABLA 6 TX")
+    print(main6TX())
+    print("TABLA 1 VX")
+    print(main1VX())
+    print("TABLA 2 VX")
+    print(main2VX())
+    print("TABLA 3 VX")
+    print(main3VX())
+    print("TABLA 4 VX")
+    print(main4VX())
+    print("TABLA 5 VX")
+    print(main5VX())
+    print("TABLA 6 VX")
+    print(main6VX())
+    
+
+
